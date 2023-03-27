a9ae46cc5ab489fab44d4b92c0fa8b57ab2d0db6
윤태준 <ta1231@ajou.ac.kr>
2023-03-27 10:31:30 +0900

    0324 코테 공부

---
+++ graph_theory/도시분할계획.py
@@ -0,0 +1,38 @@
+def find_parent(parent, x):
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+
+edges = []
+result = 0
+parent = [0] * (n + 1)
+for i in range(1, n + 1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, cost = map(int, input().split())
+    edges.append((cost, a, b))
+
+edges.sort()
+max_cost = 0
+for edge in edges:
+    cost, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        result += cost
+        union_parent(parent, a, b)
+        max_cost = max(cost, max_cost)
+
+print(result - max_cost)
